bb16a52eec8c27d1422cd81d6c49679e86c9f7bd
Tetiana Siestova <siestova.t@gmail.com>
2021-08-16 05:43:04 +0300

    HW_01 - initial commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/js/index.js", "./src/scss/style.scss"],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      images: path.resolve(__dirname, "src/assets/img/"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Index",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "post",
+      filename: "post.html",
+      template: "src/post.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "blog",
+      filename: "blog.html",
+      template: "src/blog.html",
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: ["gifsicle", ["mozjpeg", { quality: 80 }]],
+      },
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets/pic", to: "assets/pic" }],
+    }),
+  ],
+};
